4289a1437683f7ff99ae823345d70de6bda31c98
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2026-03-24 09:37:48 +0800

    [feat] added src folder

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
-import productRoutes from "./routes/product.routes";
+import productRoutes from "./routes/product.routes.js";
+
 const app = express();
 
 app.use("/api/products", productRoutes);
--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import Product from "../models/product.model";
+import Product from "../models/product.model.js";
 
 export const getProducts = async (req: Request, res: Response) => {
   const products = await Product.find();
