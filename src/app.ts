fb15f8f8997472b1f13a50e92caecda3cd600778
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2026-03-24 10:40:36 +0800

    [update] cors

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,15 @@ import productRoutes from "./routes/product.routes";
 
 const app = express();
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
+
 app.use("/api/products", productRoutes);
-app.use(cors());
+
 app.use(express.json());
 
 export default app;
