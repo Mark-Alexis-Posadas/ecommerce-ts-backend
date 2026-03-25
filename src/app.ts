e728262672a076d9cccd39995c22c4f0805c3bbe
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2026-03-25 13:26:40 +0800

    fix: auth

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import productRoutes from "./routes/product.routes";
-
+import authRoutes from "./routes/auth.routes";
 const app = express();
 
 app.use(
@@ -11,8 +11,10 @@ app.use(
   }),
 );
 
-app.use("/api/products", productRoutes);
-
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/products", productRoutes);
+app.use("/api/auth", authRoutes);
 
 export default app;
