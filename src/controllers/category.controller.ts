86ad6f2b4b738d2619b30b6295c8d4b73b0a906f
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2026-05-06 15:43:37 +0800

    feat:(categories) added delete controller and routes

--- src/controllers/category.controller.ts
+++ src/controllers/category.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import { Category } from "../models/category.model";
-
+import { Product } from "../models/product.model";
 export const createCategory = async (req: Request, res: Response) => {
   try {
     const { name } = req.body;
@@ -21,6 +21,35 @@ export const createCategory = async (req: Request, res: Response) => {
   } catch (error) {
     res.status(500).json({
       message: "Failed to create category",
+    });
+  }
+};
+
+export const deleteCategory = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+
+    // check if category exists
+    const category = await Category.findById(id);
+    if (!category) {
+      return res.status(404).json({ message: "Category not found" });
+    }
+
+    // OPTIONAL BUT IMPORTANT: check if ginagamit sa products
+    const productsUsingCategory = await Product.findOne({ category: id });
+
+    if (productsUsingCategory) {
+      return res.status(400).json({
+        message: "Cannot delete category. It is being used by products.",
+      });
+    }
+
+    await Category.findByIdAndDelete(id);
+
+    res.json({ message: "Category deleted successfully" });
+  } catch (error) {
+    res.status(500).json({
+      message: "Failed to delete category",
     });
   }
 };
--- src/routes/category.routes.ts
+++ src/routes/category.routes.ts
@@ -1,8 +1,8 @@
 import express from "express";
 import { createCategory } from "../controllers/category.controller";
-
+import { deleteCategory } from "../controllers/category.controller";
 const router = express.Router();
 
 router.post("/", createCategory);
-
+router.delete("/:id", deleteCategory);
 export default router;
